a2bac25a1fb65c85b50b4447e4f433c1b178626b
Julian Ceballos <cristianjulianceballos@gmail.com>
2010-09-03 19:00:30 -0500

    Modelos de base de datos propuestos

--- website/models.py
+++ website/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='upload/')
+    year = models.CharField(max_length=4)
+    
+class Topic(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(max_length=100)
+    topic = models.ForeignKey(Topic)
+    
+class Answer(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField()
+    question = models.ForeignKey(Question)
