e2a69d017521423323349716127e403187db2fb2
Julian Ceballos <cristianjulianceballos@gmail.com>
2010-09-21 19:14:09 -0500

    lectura/escritura de base de datos - Listo!

--- website/models.py
+++ website/models.py
@@ -8,16 +8,28 @@ class Project(models.Model):
     year = models.CharField(max_length=4)
     description = models.TextField()
     
+    def __unicode__(self):
+        return unicode(self.name)
+    
 class Topic(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, verbose_name='Titulo')
+    
+    def __unicode__(self):
+        return unicode(self.title)
     
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.TextField(max_length=100)
     topic = models.ForeignKey(Topic)
     
+    def __unicode__(self):
+        return unicode(self.text)
+    
 class Answer(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.TextField()
     question = models.ForeignKey(Question)
+    
+    def __unicode__(self):
+        return unicode(self.text)
--- website/views.py
+++ website/views.py
@@ -6,16 +6,36 @@ def inicio(request):
     return render_to_response('inicio.html',{},context_instance=RequestContext(request))
 
 def empresa(request):
-    return render_to_response('empresa.html',{})
+    return render_to_response('empresa.html',{},context_instance=RequestContext(request))
     
 def servicios(request):
-    return render_to_response('servicios.html',{})
+    return render_to_response('servicios.html',{},context_instance=RequestContext(request))
 
 def portafolio(request):
-    return render_to_response('portafolio.html',{})
+    from models import Project
+    projects = Project.objects.all()
+    print projects
+    return render_to_response('portafolio.html',{},context_instance=RequestContext(request))
     
 def faq(request):
-    return render_to_response('faq.html',{})
+    from models import Topic
+    topics = Topic.objects.all()
+    print topics
+    return render_to_response('faq.html',{},context_instance=RequestContext(request))
+
+def getQuestions(request):
+    if request.is_ajax():
+        from models import Question
+        questions = Question.objects.filter(topic__exact=request.POST['topic'])
+        print questions
+    return HttpResponse('hola')
+
+def getAnswer(request):
+    if request.is_ajax():
+        from models import Answer
+        answer = Answer.objects.get(question__exact=request.POST['question'])
+        print answer
+    return HttpResponse('hola')
     
 def contacto(request):
-    return render_to_response('contacto.html',{})
+    return render_to_response('contacto.html',{},context_instance=RequestContext(request))
