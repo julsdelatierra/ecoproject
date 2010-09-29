3791f0409ec112b1fc31720bd60b9faf1d2d4d4a
Julian Ceballos <cristianjulianceballos@gmail.com>
2010-09-28 19:07:32 -0500

    fichero lib y action agregado

--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, verbose_name='Name')
-    image = models.ImageField(upload_to='upload/', width="120px", height="120px")
+    image = models.ImageField(upload_to='upload/')
     year = models.CharField(max_length=4)
     description = models.TextField()
     
