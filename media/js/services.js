67a928b5b9cf05330385dd521ea855d3be57be1d
Carlos Maldonado <karlozm@gmail.com>
2010-11-11 09:55:22 -0600

    cambios diseno panel servicios

--- media/js/services.js
+++ media/js/services.js
@@ -4,15 +4,15 @@ var data = [
         "processed":[
             {
                 "name":"Evaluaci&oacute;n de impacto ambiental",
-                "content":"Evaluaci&oacute;n de impacto ambiental"
+                "content":"- Manifestaciones de impacto ambiental en sus distintas modalidades<br>- Evaluaci&oacute;n de da&os ambientales<br>- Cambio de uso del suelo y Estudios Técnicos Justificativos<br>- Programa de rescate y de reforestaci&oacute;n<br>- Seguimiento de cumplimiento de t&eacute;rminos y condicionantes"
             },
             {
                 "name":"Residuos peligrosos",
-                "content":"Residuos peligrosos"
+                "content":"- Alta/Baja de empresas generadoras de RP<br>- Diseño de áreas de almacenamiento de RP<br>- Procedimiento de manejo de RP<br>- Planes de manejo de RP<br>- Autorizaciones de centros de acopio y transporte de RP<br>- Autorización de tratamiento de RP<br>- Diagnósticos situacionales de RP"
             },
             {
                 "name":"Riesgo ambiental",
-                "content":"Riesgo ambiental"
+                "content":"- Estudio de Riesgo  Ambiental Nivel 01, 02 y 03<br>- Seguimiento de resolutivo de riesgo ambiental<br>- Elaboraci&oacute;n de Programas para la prevenci&oacute;n de accidentes (PPA)<br>- Seguimiento de resolutivo del PPA"
             }
         ]
     },
@@ -21,11 +21,11 @@ var data = [
         "processed":[
             {
                 "name":"Procedimientos administrativos",
-                "content":"Procedimientos administrativos"
+                "content":"- Seguimiento de medidas técnicas derivadas de inspecciones<br>- Contestación de visitas de inspecci&oacute;n<br>- Informes de Emergencia<br>- Peritajes<br>- Gesti&oacute;n para pagos de multas"
             },
             {
                 "name":"Auditoria ambiental",
-                "content":"Auditoria ambiental"
+                "content":"- Realizaci&oacute;n de Auditorias Ambientales<br>- Dictamen de conclusi&oacute;n<br>- Seguimiento planes de trabajo e informes<br>- Diagn&oacute;stico Ambiental<br>- Asesor&iacute;a y seguimiento en certificaci&oacute;n de industria limpia"
             }
         ]
     },
@@ -33,8 +33,8 @@ var data = [
         "name":"CONAGUA",
         "processed":[
             {
-                "name":"x",
-                "content":"y"
+                "name":"Gestion de Tr&aacute;mites",
+                "content":"- Obtenci&oacute;n de Título de Concesi&oacute;n para el aprovechamiento de Aguas subterr&aacute;neas<br>- Permiso de descarga de aguas residuales<br>- Informes Trimestrales o Anuales de aprovechamiento de aguas<br>- Informes Trimestrales o Anuales de descarga de aguas residuales<br>- Laboratorio de aguas"
             }
         ]
     },
@@ -42,8 +42,8 @@ var data = [
         "name":"STPS",
         "processed":[
             {
-                "name":"x",
-                "content":"y"
+                "name":"Estudios de riesgo",
+                "content":"Normas:<br>- NOM-002-STPS-2000<br>- NOM-004-STPS-1999<br>- NOM-005-STPS-1998<br>- NOM-010-STPS-1999<br>- NOM-018-STPS-2000"
             }
         ]
     },
@@ -51,8 +51,8 @@ var data = [
         "name":"Secretaria de econom&iacute;a",
         "processed":[
             {
-                "name":"x",
-                "content":"y"
+                "name":"Asesor&iacute;a para certificaci&oacute;n",
+                "content":"Certificaciones: <br>- ISO 9001<br>- ISO 14001<br>- ISO 18001"
             }
         ]
     },
@@ -60,8 +60,8 @@ var data = [
         "name":"Evaluaci&oacute;n territorial",
         "processed":[
             {
-                "name":"x",
-                "content":"y"
+                "name":"Evaluaci&oacute;n territorial",
+                "content":"- A trav&eacute;s del uso de sistemas de informaci&oacute;n geogr&aacute;fica y otras herramientas se realiza la evaluaci&oacute;n territorial que nos permite establecer las relaciones que guarda la sociedad con el espacio geogr&aacute;fico.<br> <br>- Permite incidir en las decisiones de aprovechamiento de un espacio determinado, tomando en cuenta las pol&iacute;ticas p&uacute;blicas, la vocaci&oacute;n natural del suelo, y los procesos y relaciones con la sociedad."
             }
         ]
     },
@@ -69,8 +69,8 @@ var data = [
         "name":"Capacitaci&oacute;n ambiental",
         "processed":[
             {
-                "name":"x",
-                "content":"y"
+                "name":"Capacitaci&oacute;n",
+                "content":"- Elaboraci&oacute;n de manuales de capacitaci&oacute;n<br>- Desarrollo de procedimientos de capacitación en temas de:<br><ul><li>Aire</li><li>Agua</li><li>Suelo y subsuelo</li><li>Ruido</li><li>Residuos s&oacute;lidos urbanos, de manejo especial y peligrosos</li><li>Seguridad e Higiene</li><li>Manejo de sustancias qu&iacute;micas</li></ul>"
             }
         ]
     }
